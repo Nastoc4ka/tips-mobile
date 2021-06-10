4e4de9ac4bca1b84ad47b695c120dfa3bdd4a75f
Anastasiia Bilytska <nastoc4ka@gmail.com>
2021-06-10 18:21:36 +0300

    change name react-native tips_mobile add devtools redux

---
+++ tips_mobile/src/services/client.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:8080/api';
+//localhost
+//10.100.3.147
+const client = axios.create({
+    baseURL
+});
+
+const catchError = (errorType) => (err) => {
+    if (err.response && err.response.data && err.response.data.msg) {
+        throw new errorType(err.response.data.msg);
+    }
+    throw new errorType('Something went wrong... please contact vendor');
+};
+
+export {
+    client,
+    catchError,
+}
+
