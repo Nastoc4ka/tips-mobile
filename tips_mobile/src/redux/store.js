4fab01730569f1b673fe7ddcbc6bd066f472c81d
Anastasiia Bilytska <nastoc4ka@gmail.com>
2021-06-10 18:21:36 +0300

    change name react-native tips_mobile add devtools redux

---
+++ tips_mobile/src/redux/store.js
@@ -0,0 +1,18 @@
+import {applyMiddleware, createStore} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {rootReducer} from './reducers/rootReducer';
+import sagaWatcher from '../saga';
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+    )
+);
+
+sagaMiddleware.run(sagaWatcher);
+
+export default store
