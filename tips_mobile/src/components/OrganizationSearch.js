07801df239287fc0a789de67550df6b8ee7fdd4b
Anastasiia Bilytska <nastoc4ka@gmail.com>
2021-06-10 18:21:36 +0300

    change name react-native tips_mobile add devtools redux

---
+++ tips_mobile/src/components/OrganizationSearch.js
@@ -0,0 +1,132 @@
+import React, { useRef, useState, useEffect } from 'react';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import { getOrganisationsSaga } from '../redux/actions';
+import {Input, SearchDropDown, IconInInputView} from './index'
+
+const OrganizationSearch = ({error, setOrganizationInData}) => {
+    const dispatch = useDispatch();
+    const {organisations} = useSelector(state => state.authRegisterReducer);
+    const organizationInputRef = useRef(null);
+    const [searching, setSearching] = useState(false);
+    const [focus, setFocus] = useState(false);
+    const [filtered, setFiltered] = useState(organisations);
+    const [organization, setOrganization] = useState({
+        name: '',
+        id: ''
+    });
+
+    const closeSearchOrganization = () => {
+        if (!organization.id) {
+            setOrganization({
+                name: '',
+                id: ''
+            });
+            setOrganizationInData('', '')
+        }
+        setFocus(false);
+        setSearching(false);
+        organizationInputRef.current.blur();
+    };
+
+    const handleSearch = (text) => {
+        setOrganizationInData('', '');
+        setOrganization({
+            name: text,
+            id: ''
+        });
+
+        if (text.trim().length > 2) {
+            setSearching(true);
+            const tempList = organisations.filter(item => {
+                const itemToCheck = String(`${item.name} (${item.address})`).toLowerCase();
+                const newString = String(text.toLowerCase());
+                if (itemToCheck.startsWith(newString)) {
+                    return item
+                }
+            });
+            setFiltered(tempList);
+        } else {
+            setSearching(false);
+            console.log(organisations, "org");
+            // setFiltered(organisations);
+        }
+    };
+
+    const handleChoosingOrganization = (id, nameAndAddress) => {
+        setOrganizationInData(id, nameAndAddress);
+        setOrganization({
+            name: nameAndAddress,
+            id
+        });
+        setSearching(false);
+        setFocus(false);
+        organizationInputRef.current.blur();
+    };
+
+    useEffect(() => {
+        dispatch(getOrganisationsSaga());
+    }, []);
+
+    return (
+        <View style={focus ? styles.container : null}>
+            {focus 
+                ? <TouchableOpacity style={styles.btn} onPress={closeSearchOrganization}>
+                    <Text style={styles.text}>x</Text>
+                </TouchableOpacity> 
+                : null
+            }
+            
+            <Input
+                label='Заведение'
+                onFocus={() => setFocus(true)}
+                refs={organizationInputRef}
+                value={organization.name}
+                maxLength={60}
+                message={error}
+                handleChange={handleSearch}
+            >
+                <TouchableOpacity onPress={() => setOrganization({
+                    name: '',
+                    id: ''
+                })}>
+                    <IconInInputView>
+                        {focus && organisations ? <Text style={styles.text}>x</Text> : null}
+                    </IconInInputView>
+                </TouchableOpacity>
+            </Input>
+
+            {searching 
+                ? <SearchDropDown
+                    onPress={(id, nameAndAddress) => handleChoosingOrganization(id, nameAndAddress)}
+                    dataSource={filtered}
+                /> 
+                : null
+            }
+        </View>
+
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#fff',
+        zIndex: 1,
+        top: 0
+    },
+    btn: {
+        marginLeft: 5,
+        marginBottom: 5,
+    },
+    text: {
+        color: 'grey',
+        fontWeight: '800',
+        fontSize: 20,
+        justifyContent: 'center'
+    }
+});
+
+export default OrganizationSearch;
---
+++ tips_mobile/src/screens/authentication/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import BiometricPopup from './Biometric';
+import Password from './Password';
+
+const Authentication = () => {
+    return (
+        <View>
+            <StatusBar
+                animated={true}
+                backgroundColor='#00A03E'
+                barStyle='light-content'
+                showHideTransition='fade'
+            />
+            <Password />
+        </View>
+    )
+}
+
+export default Authentication;
