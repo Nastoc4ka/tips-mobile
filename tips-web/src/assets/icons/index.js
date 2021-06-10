904e9cfec198d1dc24802085b36e139547c15def
Lina_Karyzhska <lina.dronova.s@gmail.com>
2021-06-10 13:22:58 +0300

    started web project

---
+++ tips-web/src/assets/icons/index.js
@@ -0,0 +1,39 @@
+import Icn_qrcode from './icn_qrcode.svg';
+import Icn_revievs from './icn_revievs.svg';
+import Icn_settings from './icn_settings.svg';
+import Icn_tips from './icn_tips.svg';
+import Icn_home from './icn_home.svg';
+import Logo from './logo.svg';
+import Back from './back.svg';
+import Icn_filters from './icn_filters.svg';
+import Flag_huge from './flag_huge.svg';
+import Flag_petite from './flag_petite.svg';
+import Icn_arrow from './icn_arrow.svg';
+import Reaction_huge from './reaction_huge.svg';
+import Reaction_petite from './reaction_petite.svg';
+import Dot from './dot.svg';
+import Arrow_left from './arrow_left.svg';
+import Arrow_right from './arrow_right.svg';
+import VisibilityHide from './visibilityHide.svg';
+import VisibilityShow from './visibilityShow.svg';
+
+export {
+    Icn_home,
+    Icn_qrcode,
+    Icn_revievs,
+    Icn_settings,
+    Icn_tips,
+    Logo,
+    Back,
+    Icn_filters,
+    Flag_huge,
+    Icn_arrow,
+    Flag_petite,
+    Reaction_huge,
+    Reaction_petite,
+    Dot,
+    Arrow_left,
+    Arrow_right,
+    VisibilityHide,
+    VisibilityShow,
+}
---
+++ tips-web/src/saga/saga.js
@@ -0,0 +1,12 @@
+import {call, put, takeEvery} from 'redux-saga/effects';
+import {
+
+} from '../redux/actions';
+
+import {
+    
+} from "../redux/actions/types";
+
+export function* sagaWatcher() {
+    
+}
