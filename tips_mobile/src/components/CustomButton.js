8567a4b2f9dbe32c58975d4a4a2fe212da69fad7
Anastasiia Bilytska <nastoc4ka@gmail.com>
2021-06-14 11:13:06 +0300

    Add buttons to settings screen

--- tips_mobile/src/components/CustomButton.js
+++ tips_mobile/src/components/CustomButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const BackButton = ({ title, styles, onPress }) => {
+const BackButton = ({ title, styles, onPress, leftIcon = null }) => {
     return (
         <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={onPress}>
+            {leftIcon}
             <Text style={styles.text}>{title}</Text>
         </TouchableOpacity>
     )
