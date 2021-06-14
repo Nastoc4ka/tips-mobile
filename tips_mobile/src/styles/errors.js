a59fa10274ee2ffc48fa656faa4e1bb0c50dd8d0
Anastasiia Bilytska <nastoc4ka@gmail.com>
2021-06-14 16:59:53 +0300

    add securityTextEntry default true refactor signUp errors

--- tips_mobile/src/styles/errors.js
+++ tips_mobile/src/styles/errors.js
@@ -4,10 +4,8 @@ export const errors = StyleSheet.create({
     wrapper: {
         //flex: 1,
         alignItems: 'flex-end',
-        height: 12
     },
     text: {
-        lineHeight: 12,
         fontSize: 12,
         color: 'red',
     }
